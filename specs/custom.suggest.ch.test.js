89ea605bc6377c0501c934c9ed081c7efd740a43
antonzubar <anton-zubar@yandex.ru>
2018-02-04 22:09:38 +0300

    finished test. async problem has left.

--- specs/custom.suggest.ch.test.js
+++ specs/custom.suggest.ch.test.js
@@ -12,21 +12,63 @@ describe('Custom scores for Input: ', function () {
     it('user lands on Input page', function () {
         allure.feature('Input page');
         page = new InputPage(page);
-        browser.executeScript('window.scrollTo(1300, 900);').then(function(){
-            page.scoreQuality.getText().then(function(text){
+
+        //Scroll down and verify that initial values are 0.
+        browser.executeScript('window.scrollTo(100000, 100000);').then(function () {
+            page.scoreQuality.getText().then(function (text) {
+                expect(text).toBe('0.0');
+            });
+            page.scoreCondition.getText().then(function (text) {
+                expect(text).toBe('0.0');
+            });
+            page.scoreLocation.getText().then(function (text) {
                 expect(text).toBe('0.0');
             });
         });
-        page.scoreCondition.getText().then(function(text){
-            expect(text).toBe('0.0');
+
+        //Set custom QCL values by moving sliders
+        browser.actions().dragAndDrop(
+            page.scoreQualitySlider,
+            {x: 22, y: 0}
+        ).perform();
+
+        browser.actions().dragAndDrop(
+            page.scoreConditionSlider,
+            {x: 35, y: 0}
+        ).perform();
+
+        browser.actions().dragAndDrop(
+            page.scoreLocationSlider,
+            {x: 75, y: 0}
+        ).perform();
+
+        //remember new QCL values
+        var manualQuality, manualCondition, manualLocation;
+        page.scoreQualitySlider.getAttribute('aria-valuenow').then(function (text) {
+            manualQuality = text;
         });
-        page.scoreLocation.getText().then(function(text){
-            expect(text).toBe('0.0');
+        page.scoreConditionSlider.getAttribute('aria-valuenow').then(function (text) {
+            manualCondition = text;
+        });
+        page.scoreLocationSlider.getAttribute('aria-valuenow').then(function (text) {
+            manualLocation = text;
         });
 
-        page.editScores.click();
-        page.setValueOfField(page.scoreQualityField, 2);
-        page.setValueOfField(page.scoreConditionField, 3);
-        page.setValueOfField(page.scoreLocationField, 4);
-    });
+        //click save and verify that changes are saved
+        page.saveQCL.click();
+        browser.waitForAngular();
+
+        var EC = protractor.ExpectedConditions;
+        browser.wait(EC.textToBePresentInElement(page.scoreQuality, manualQuality), 5000).then(function(){
+            page.scoreQuality.getText().then(function (text) {
+                expect(text).toBe(manualQuality);
+            });
+            page.scoreCondition.getText().then(function (text) {
+                expect(text).toBe(manualCondition);
+            });
+            page.scoreLocation.getText().then(function (text) {
+                expect(text).toBe(manualLocation);
+            });
+        });
+    }, 24000);
 });
