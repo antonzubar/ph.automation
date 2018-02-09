06b9ec84fe1e0f014c3e4216478388d1f9f95aa4
antonzubar <anton-zubar@yandex.ru>
2018-02-09 18:03:09 +0300

    update.

--- specs/pages/immissions.page.js
+++ specs/pages/immissions.page.js
@@ -4,7 +4,8 @@ var ImmissionsPage = function () {};
 
  ImmissionsPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
-     immissionsLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }}
+     immissionsLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
+     immissionsScore: { get: function () { return element.all(by.css('[ng-if="immisionsScore"]')); }}
 });
 
 module.exports = ImmissionsPage;
