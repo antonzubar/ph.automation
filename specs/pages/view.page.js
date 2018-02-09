46574900e1dde41985db47fdfa845b5a0749a5a7
antonzubar <anton-zubar@yandex.ru>
2018-02-09 18:03:55 +0300

    Update.

--- specs/pages/view.page.js
+++ specs/pages/view.page.js
@@ -4,7 +4,8 @@ var ViewPage = function () {};
 
  ViewPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
-     viewLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }}
+     viewLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
+     viewScore: { get: function () { return element.all(by.css('[ng-if="viewScore"]')); }}
 });
 
 module.exports = ViewPage;
