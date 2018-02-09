0a5a12c7570d9f316e65c6e491b1674ab9a84db9
antonzubar <anton-zubar@yandex.ru>
2018-02-09 14:44:15 +0300

    Add cards web element

--- specs/pages/similar.offers.page.js
+++ specs/pages/similar.offers.page.js
@@ -4,7 +4,8 @@ var SimilarOffersPage = function () {};
 
  SimilarOffersPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
-     similarOffersLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }}
+     similarOffersLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
+     similarCard: { get: function () { return element.all(by.css('[ng-click="$ctrl.activateCard()"]')); }}
 });
 
 module.exports = SimilarOffersPage;
