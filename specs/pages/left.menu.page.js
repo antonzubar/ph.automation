75ca418605fc6454a4f54e138013b135c4a8c82b
antonzubar <anton-zubar@yandex.ru>
2018-02-09 18:03:19 +0300

    Update.

--- specs/pages/left.menu.page.js
+++ specs/pages/left.menu.page.js
@@ -5,7 +5,8 @@ var LeftMenu = function () {};
  LeftMenu.prototype = Object.create({}, {
     //---------------------------Navigation Items-------------------------------------------------
     //Navigation bar for Switzerland
-     menuItems: { get: function () { return element.all(by.className('dib-nav__menu-link layout-wrap ng-isolate-scope layout-align-center-center layout-row')); }}
+     menuItems: { get: function () { return element.all(by.className('dib-nav__menu-link layout-wrap ng-isolate-scope layout-align-center-center layout-row')); }},
+     viewMapbutton: { get: function () { return element(by.css('[class="mapboxgl-ctrl-icon mapboxgl-ctrl-viewMap"]')); }}
 });
 
 module.exports = LeftMenu;
