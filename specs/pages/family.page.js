d0a0e27f487e5e3fcb0e20e2a1561bce5ef7779a
antonzubar <anton-zubar@yandex.ru>
2018-02-09 18:02:57 +0300

    update.

--- specs/pages/family.page.js
+++ specs/pages/family.page.js
@@ -5,7 +5,8 @@ var FamilyPage = function () {};
 //comment
  FamilyPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
-     familyLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }}
+     familyLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
+     familyScore: { get: function () { return element.all(by.css('[ng-if="familyScore"]')); }}
 });
 
 module.exports = FamilyPage;
