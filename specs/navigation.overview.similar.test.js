79c2954c1707ee36f8ef0a87ec0017f9c2f1f1e7
antonzubar <anton-zubar@yandex.ru>
2018-02-21 17:17:05 +0300

    Rework test. Added EC. Move all expectation to separate "it" blocks.

--- specs/navigation.overview.similar.test.js
+++ specs/navigation.overview.similar.test.js
@@ -7,14 +7,15 @@ var SimilarOffersPage = require('./pages/similar.offers.page');
 
 describe('Navigating from Overvieiw to Similar page: ', function () {
     var page = new LoginPage();
+    var EC = protractor.ExpectedConditions;
 
-    beforeEach(function () {
+    beforeAll(function () {
         page.typeLogin(users_data[0].UserName);
         page.typePassword(users_data[0].Password);
         page.loginToDash();
     });
 
-    it(' User clicks on all buttons under Offers area', function () {
+    it(' User clicks Active button in Offers widget', function () {
         allure.feature('Input page');
         page = new InputPage(page);
 
@@ -26,8 +27,7 @@ describe('Navigating from Overvieiw to Similar page: ', function () {
 
         //Scroll down and verify that initial values are 0.
         browser.executeScript('window.scrollTo(100000, 100000);').then(function () {
-            page.editScores.click();
-            page.saveQCL.click();
+            browser.wait(EC.elementToBeClickable(page.getValuation), 5000);
         });
         page.getValuation.click().then(function () {
             page = new OverviewPage(page);
@@ -41,31 +41,39 @@ describe('Navigating from Overvieiw to Similar page: ', function () {
                     page.menuItems.get(1).click();
                 })
             });
-
-            //Navigate to Historical Offers
-            page.offersInArea.get(1).click().then(function () {
-                page = new SimilarOffersPage(page);
-                page.similarOffersLabel.getText().then(function (text) {
-                    expect(String(text)).toBe('Ähnliche Angebote');
-                    page = new LeftMenu(page);
-                    page.menuItems.get(1).click();
-                })
-            });
-
-            //Navigate to Similar Offers page
-            page.similarGoExplore.click().then(function () {
-                page = new SimilarOffersPage(page);
-                page.similarOffersLabel.getText().then(function (text) {
-                    expect(String(text)).toBe('Ähnliche Angebote');
-                    page = new LeftMenu(page);
-                    page.menuItems.get(1).click();
-                    page = new InputPage(page);
-                })
-            });
         });
     }, 240000);
 
-    afterEach(function () {
+    it('User clicks Historic button in Offers widget', function () {
+        //Navigate to Historical Offers
+        page = new OverviewPage(page);
+        
+        page.offersInArea.get(1).click().then(function () {
+            page = new SimilarOffersPage(page);
+            page.similarOffersLabel.getText().then(function (text) {
+                expect(String(text)).toBe('Ähnliche Angebote');
+                page = new LeftMenu(page);
+                page.menuItems.get(1).click();
+            })
+        });
+    }, 10000);
+
+    it('Navigation to Similar offers', function () {
+        page = new OverviewPage(page);
+        
+        //Navigate to Similar Offers page
+        page.similarGoExplore.click().then(function () {
+            page = new SimilarOffersPage(page);
+            page.similarOffersLabel.getText().then(function (text) {
+                expect(String(text)).toBe('Ähnliche Angebote');
+                page = new LeftMenu(page);
+                page.menuItems.get(1).click();
+                page = new InputPage(page);
+            })
+        }, 10000);
+    });
+
+    afterAll(function () {
         page = new InputPage(page);
         page.logout();
         page = new LoginPage();
