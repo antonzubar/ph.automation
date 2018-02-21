0d45d45b921a0bda727fa8a9363b0740fb640c40
antonzubar <anton-zubar@yandex.ru>
2018-02-21 12:32:31 +0300

    Change score element.

--- specs/pages/noise.page.js
+++ specs/pages/noise.page.js
@@ -5,7 +5,7 @@ var NoisePage = function () {};
  NoisePage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
      noiseLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
-     quietScore: { get: function () { return element.all(by.css('[ng-if="quietScore"]')); }}
+     quietScore: { get: function () { return element(by.css('[ng-if="quietScore"]')); }}
 });
 
 module.exports = NoisePage;
