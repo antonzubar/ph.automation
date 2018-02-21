c7d07b95064eea849816531f4b9b526a3b0ca189
antonzubar <anton-zubar@yandex.ru>
2018-02-21 12:32:37 +0300

    Change score element.

--- specs/pages/shopping.page.js
+++ specs/pages/shopping.page.js
@@ -5,7 +5,7 @@ var ShoppingPage = function () {};
  ShoppingPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
      shoppingLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
-     shoppingScore: { get: function () { return element.all(by.css('[ng-if="shoppingScore"]')); }}
+     shoppingScore: { get: function () { return element(by.css('[ng-if="shoppingScore"]')); }}
 });
 
 module.exports = ShoppingPage;
