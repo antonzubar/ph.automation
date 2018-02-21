1e2543fa54bb60e8586f6280f410e2e8b01c4e9a
antonzubar <anton-zubar@yandex.ru>
2018-02-21 12:32:48 +0300

    Change score element.

--- specs/pages/immissions.page.js
+++ specs/pages/immissions.page.js
@@ -5,7 +5,7 @@ var ImmissionsPage = function () {};
  ImmissionsPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
      immissionsLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
-     immissionsScore: { get: function () { return element.all(by.css('[ng-if="immisionsScore"]')); }}
+     immissionsScore: { get: function () { return element(by.css('[ng-if="immisionsScore"]')); }}
 });
 
 module.exports = ImmissionsPage;
