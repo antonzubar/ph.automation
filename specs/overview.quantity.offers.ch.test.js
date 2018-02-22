c612456efe65ad7f84a8d09abd05ab32a420013f
antonzubar <anton-zubar@yandex.ru>
2018-02-22 13:22:24 +0300

    Rework test. Moved all expectations into separate "it" blocks.

--- specs/overview.quantity.offers.ch.test.js
+++ specs/overview.quantity.offers.ch.test.js
@@ -7,14 +7,15 @@ var SimilarOffersPage = require('./pages/similar.offers.page');
 
 describe(' Comparing offers quantity between Overview and Similar Offers pages', function () {
     var page = new LoginPage();
+    var EC = protractor.ExpectedConditions;
 
-    beforeEach(function () {
+    beforeAll(function () {
         page.typeLogin(users_data[0].UserName);
         page.typePassword(users_data[0].Password);
         page.loginToDash();
     });
 
-    it(' User clicks on all buttons under Offers area', function () {
+    it(' User clicks on Active Offers and compare value between Overview and Offers', function () {
         allure.feature('Input page');
         page = new InputPage(page);
 
@@ -26,9 +27,7 @@ describe(' Comparing offers quantity between Overview and Similar Offers pages',
 
         //Scroll down and verify that initial values are 0.
         browser.executeScript('window.scrollTo(100000, 100000);').then(function () {
-            page.editScores.click();
-            page.saveQCL.click();
-            browser.waitForAngular();
+            browser.wait(EC.elementToBeClickable(page.getValuation), 5000);
         });
         page.getValuation.click().then(function () {
             page = new OverviewPage(page);
@@ -50,39 +49,40 @@ describe(' Comparing offers quantity between Overview and Similar Offers pages',
                         }
                         expect(String(quantityOfActiveOffers)).toBe("(" + size + object);
                     });
-
-                    page = new LeftMenu(page);
-                    page.menuItems.get(1).click();
                 })
             });
 
-            //Navigate to Historical Offers
-            var quantityOfHistoricalOffers;
-            page.offersQuantityInArea.get(1).getText().then(function (text) {
-                quantityOfHistoricalOffers = text;
-            });
-            page.offersInArea.get(1).click().then(function () {
-                page = new SimilarOffersPage(page);
-                page.similarOffersLabel.getText().then(function (text) {
-                    page.similarCard.count().then(function (size) {
-                        var object;
-                        if (size == 1) {
-                            object = " Objekt)"
-                        } else {
-                            object = " Objekte)"
-                        }
-                        expect(String(quantityOfHistoricalOffers)).toBe("(" + size + object);
-                    });
-
-                    page = new LeftMenu(page);
-                    page.menuItems.get(1).click();
-                    page = new InputPage(page);
-                })
-            });
         });
     }, 240000);
 
-    afterEach(function () {
+    it(' User clicks on Historical Offers and compare value between Overview and Offers', function () {
+
+        page = new LeftMenu(page);
+        page.menuItems.get(1).click();
+        page = new OverviewPage(page);
+
+        //Navigate to Historical Offers
+        var quantityOfHistoricalOffers;
+        page.offersQuantityInArea.get(1).getText().then(function (text) {
+            quantityOfHistoricalOffers = text;
+        });
+        page.offersInArea.get(1).click().then(function () {
+            page = new SimilarOffersPage(page);
+            page.similarOffersLabel.getText().then(function (text) {
+                page.similarCard.count().then(function (size) {
+                    var object;
+                    if (size == 1) {
+                        object = " Objekt)"
+                    } else {
+                        object = " Objekte)"
+                    }
+                    expect(String(quantityOfHistoricalOffers)).toBe("(" + size + object);
+                });
+            })
+        }, 20000);
+    });
+
+    afterAll(function () {
         page = new InputPage(page);
         page.logout();
         page = new LoginPage();
