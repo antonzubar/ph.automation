31d4c87341c657bedac81594b42cefdadb3d97bd
antonzubar <anton-zubar@yandex.ru>
2018-02-22 17:06:46 +0300

    Add function with EC to wait Scores.

--- specs/pages/view.page.js
+++ specs/pages/view.page.js
@@ -1,11 +1,16 @@
 'use strict';
 
 var ViewPage = function () {};
+var EC = protractor.ExpectedConditions;
 
  ViewPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
      viewLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
-     viewScore: { get: function () { return element(by.css('[ng-if="viewScore"]')); }}
+     viewScore: { get: function () { return element(by.css('[ng-if="viewScore"]')); }},
+     waitForScore: { value: function (webelement, scoreValue) {
+            browser.wait(EC.textToBePresentInElement(webelement, scoreValue), 15000);
+        }
+     }
 });
 
 module.exports = ViewPage;
