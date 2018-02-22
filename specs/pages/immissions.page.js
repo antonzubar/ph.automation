9698d4dbcddead6e052c0981a10d5d1cb08dfedf
antonzubar <anton-zubar@yandex.ru>
2018-02-22 17:06:58 +0300

    Add function with EC to wait Scores.

--- specs/pages/immissions.page.js
+++ specs/pages/immissions.page.js
@@ -1,11 +1,16 @@
 'use strict';
 
 var ImmissionsPage = function () {};
+var EC = protractor.ExpectedConditions;
 
  ImmissionsPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
      immissionsLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
-     immissionsScore: { get: function () { return element(by.css('[ng-if="immisionsScore"]')); }}
+     immissionsScore: { get: function () { return element(by.css('[ng-if="immisionsScore"]')); }},
+     waitForScore: { value: function (webelement, scoreValue) {
+            browser.wait(EC.textToBePresentInElement(webelement, scoreValue), 15000);
+        }
+     }
 });
 
 module.exports = ImmissionsPage;
