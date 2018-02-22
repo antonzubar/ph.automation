7096adcbb1c2e77b9cc191368fe82c4e4adcc6c5
antonzubar <anton-zubar@yandex.ru>
2018-02-22 17:06:36 +0300

    Add function with EC to wait Scores.

--- specs/pages/noise.page.js
+++ specs/pages/noise.page.js
@@ -1,11 +1,16 @@
 'use strict';
 
 var NoisePage = function () {};
+var EC = protractor.ExpectedConditions;
 
  NoisePage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
      noiseLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
-     quietScore: { get: function () { return element(by.css('[ng-if="quietScore"]')); }}
+     quietScore: { get: function () { return element(by.css('[ng-if="quietScore"]')); }},
+     waitForScore: { value: function (webelement, scoreValue) {
+            browser.wait(EC.textToBePresentInElement(webelement, scoreValue), 15000);
+        }
+     }
 });
 
 module.exports = NoisePage;
