5100435c06a9c61a3a91b4b557216a0b268382d4
antonzubar <anton-zubar@yandex.ru>
2018-02-22 17:07:04 +0300

    Add function with EC to wait Scores.

--- specs/pages/shopping.page.js
+++ specs/pages/shopping.page.js
@@ -1,11 +1,16 @@
 'use strict';
 
 var ShoppingPage = function () {};
+var EC = protractor.ExpectedConditions;
 
  ShoppingPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
      shoppingLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
-     shoppingScore: { get: function () { return element(by.css('[ng-if="shoppingScore"]')); }}
+     shoppingScore: { get: function () { return element(by.css('[ng-if="shoppingScore"]')); }},
+     waitForScore: { value: function (webelement, scoreValue) {
+            browser.wait(EC.textToBePresentInElement(webelement, scoreValue), 15000);
+        }
+     }
 });
 
 module.exports = ShoppingPage;
