50c3756cefaffa8116e4c32abaa142132a6d26d1
antonzubar <anton-zubar@yandex.ru>
2018-02-22 17:07:10 +0300

    Add function with EC to wait Scores.

--- specs/pages/family.page.js
+++ specs/pages/family.page.js
@@ -1,12 +1,17 @@
 'use strict';
 
 var FamilyPage = function () {};
+var EC = protractor.ExpectedConditions;
 
 //comment
  FamilyPage.prototype = Object.create({}, {
     //---------------------------Info Pane items-------------------------------------------------
      familyLabel: { get: function () { return element.all(by.css('[ng-if="!$root.confirmationStarted"]')); }},
-     familyScore: { get: function () { return element(by.css('[ng-if="familyScore"]')); }}
+     familyScore: { get: function () { return element(by.css('[ng-if="familyScore"]')); }},
+     waitForScore: { value: function (webelement, scoreValue) {
+            browser.wait(EC.textToBePresentInElement(webelement, scoreValue), 15000);
+        }
+     }
 });
 
 module.exports = FamilyPage;
