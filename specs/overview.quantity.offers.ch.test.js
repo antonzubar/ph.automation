43d9460cfff1ac4a85d6570ae16b444ae8e4fbaa
antonzubar <anton-zubar@yandex.ru>
2018-02-26 11:54:20 +0300

    Changed address.

--- specs/overview.quantity.offers.ch.test.js
+++ specs/overview.quantity.offers.ch.test.js
@@ -19,7 +19,7 @@ describe(' Comparing offers quantity between Overview and Similar Offers pages',
         allure.feature('Input page');
         page = new InputPage(page);
 
-        page.typeAddress("Hardgutstrasse 22, 8048 Zürich, Switzerland");
+        page.typeAddress("Pfingstweidstrasse 51, 8005 Zürich, Switzerland");
         page.setValueOfField(page.buildingYear, page.getRandomInteger("1990"));
         page.setValueOfField(page.livingArea, page.getRandomInteger("60"));
         page.setValueOfField(page.floorNumber, page.getRandomInteger("5"));
