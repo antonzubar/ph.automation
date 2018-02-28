7bc276c8a92ce79855560fbc43a2f6c4efdef5a4
antonzubar <anton-zubar@yandex.ru>
2018-02-28 11:28:35 +0300

    Removed app data (labels, titles etc) from test.

--- specs/login.swiss.test.js
+++ specs/login.swiss.test.js
@@ -4,37 +4,40 @@ var LoginPage = require('./pages/login.page');
 var InputPage = require('./pages/input.page');
 var using = require('jasmine-data-provider');
 var testdata = require('./test_data/users.testdata.js');
+var loginAppData = require('./app_data/login.appdata.js');
 
-
-using(testdata, function (data) {
-    describe(' Login page ' + data.countryCode + ' user: ', function () {
-        var page = new LoginPage();
-        it(data.UserName + 'user lands on Login page', function () {
-            allure.feature('Login/Logout feature');
-            expect(browser.getTitle()).toEqual('Login | dash');
-        });
-
-        it(data.UserName + ' user logins to Swiss dash', function () {
-            allure.feature('Login/Logout feature');
-            page.typeLogin(data.UserName);
-            page.typePassword(data.Password);
-            page.loginToDash();
-
-            expect(browser.getTitle()).toEqual(data.PageTitle);
-        });
-
-        it(data.UserName + ' Check that address is default ', function () {
-            page = new InputPage(page);
-            page.getValue(page.address).then(function (text) {
-                expect(text).toEqual(data.DefaultAddress);
+using(loginAppData, function (app_data) {
+    using(testdata, function (data) {
+        describe(' Login page ' + data.countryCode + ' user: ', function () {
+            var page = new LoginPage();
+            it(data.UserName + 'user lands on Login page', function () {
+                allure.feature('Login/Logout feature');
+                expect(browser.getTitle()).toEqual(app_data.PageTitle);
             });
-        });
 
-        it(data.UserName + ' user is logged out', function () {
-            allure.feature('Login/Logout feature');
-            page.logout();
-            page = new LoginPage();
-            expect(page.loginForm.isPresent()).toBe(true);
+            it(data.UserName + ' user logins to Swiss dash', function () {
+                allure.feature('Login/Logout feature');
+                page.typeLogin(data.UserName);
+                page.typePassword(data.Password);
+                page.loginToDash();
+
+                expect(browser.getTitle()).toEqual(data.PageTitle);
+            });
+
+            it(data.UserName + ' Check that address is default ', function () {
+                page = new InputPage(page);
+                page.getValue(page.address).then(function (text) {
+                    expect(text).toEqual(data.DefaultAddress);
+                });
+            });
+
+            it(data.UserName + ' user is logged out', function () {
+                allure.feature('Login/Logout feature');
+                page.logout();
+                page = new LoginPage();
+                expect(page.loginForm.isPresent()).toBe(true);
+            });
         });
     });
 });
+
