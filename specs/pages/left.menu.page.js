6d7e95d5a02a97e5312ce736bb558baf876b37b5
antonzubar <anton-zubar@yandex.ru>
2018-02-28 10:16:52 +0300

    Add left menu webelements.

--- specs/pages/left.menu.page.js
+++ specs/pages/left.menu.page.js
@@ -6,7 +6,25 @@ var LeftMenu = function () {};
     //---------------------------Navigation Items-------------------------------------------------
     //Navigation bar for Switzerland
      menuItems: { get: function () { return element.all(by.className('dib-nav__menu-toggle ng-scope')); }},
-     viewMapbutton: { get: function () { return element(by.css('[class="mapboxgl-ctrl-icon mapboxgl-ctrl-viewMap"]')); }}
+     viewMapbutton: { get: function () { return element(by.css('[class="mapboxgl-ctrl-icon mapboxgl-ctrl-viewMap"]')); }},
+
+     //Menu items
+     menuInput: { get: function () { return element(by.xpath('//span[contains(text(), " Eingabe ")]')); }},
+     menuOverview: { get: function () { return element(by.xpath('//span[contains(text(), " Überblick ")]')); }},
+     menuSimilar: { get: function () { return element(by.xpath('//span[contains(text(), " Ähnlich ")]')); }},
+     menuSimilarOverview: { get: function () { return element(by.xpath('//span[contains(text(), " Überblick ")]')); }},
+     menuSimilarOffers: { get: function () { return element(by.xpath('//span[contains(text(), " Angebote ")]')); }},
+     menuMarket: { get: function () { return element(by.xpath('//span[contains(text(), " Markt ")]')); }},
+     menuEnvironment: { get: function () { return element(by.xpath('//span[contains(text(), " Umwelt ")]')); }},
+     menuNoise: { get: function () { return element(by.xpath('//span[contains(text(), " Geräusch ")]')); }},
+     menuView: { get: function () { return element(by.xpath('//span[contains(text(), " Aussicht ")]')); }},
+     menuImmisions: { get: function () { return element(by.xpath('//span[contains(text(), " Immissionen ")]')); }},
+     menuDailyNeeds: { get: function () { return element(by.xpath('//span[contains(text(), " Tägliche Bedürfnisse ")]')); }},
+     menuShopping: { get: function () { return element(by.xpath('//span[contains(text(), " Einkaufen ")]')); }},
+     menuFamily: { get: function () { return element(by.xpath('//span[contains(text(), " Familie ")]')); }},
+     menuTransport: { get: function () { return element(by.xpath('//span[contains(text(), " Transport ")]')); }},
+     menuMyDash: { get: function () { return element(by.xpath('//span[contains(text(), " My Dash ")]')); }}
+
 });
 
 module.exports = LeftMenu;
